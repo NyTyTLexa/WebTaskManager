a797836dd3bd564c33b1ee5f90d3ea10ddaf1486
Ardor <Ardor@DESKTOP-0UNVM7R>
2024-11-16 16:22:24 +0500

    Редактирование функции регистрации, изменение интерфейса для основного окна

---
+++ StartFull.bat
@@ -0,0 +1,3 @@
+start "" "%~dp0DesktopTaskManager\bin\Debug\net8.0-windows\DesktopTaskManager.exe"
+start "" "%~dp0Web\Web\bin\Debug\net8.0\Web.exe"
+start "" "%~dp0WebApplication1\bin\Debug\net8.0\WebApi.exe"
