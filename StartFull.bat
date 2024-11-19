297fbad0e748b6ca592ce958581d341dfa772b74
Ardor <Ardor@DESKTOP-0UNVM7R>
2024-11-19 20:27:07 +0500

    Добавление окон для CRUD

--- StartFull.bat
+++ StartFull.bat
@@ -1,3 +1,3 @@
 start "" "%~dp0DesktopTaskManager\bin\Debug\net8.0-windows\DesktopTaskManager.exe"
-start "" "%~dp0Web\Web\bin\Debug\net8.0\Web.exe"
 start "" "%~dp0WebApplication1\bin\Debug\net8.0\WebApi.exe"
+start "" "C:\Users\Ardor\source\repos\WebTaskManager\Web\Web\bin\Release\net8.0\Web.exe"
